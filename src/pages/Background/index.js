090c11a71b737351e3eec3e6257172d4c6c3aec9
EloiseRosen <eloise.rosen@gmail.com>
2025-03-21 22:52:56 -0700

    functionality for a pre-set tab snooze time interval

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,22 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+chrome.alarms.onAlarm.addListener(async (alarm) => { // listen for alarms
+  if (!alarm.name.startsWith('snoozedTab_')) {
+    return; 
+  }
+
+  try {
+    const snoozeId = alarm.name;
+
+    const result = await chrome.storage.local.get(snoozeId);
+    const snoozeInfo = result[snoozeId];
+
+    if (snoozeInfo && snoozeInfo.url) {
+      // reopen the tab
+      await chrome.tabs.create({ url: snoozeInfo.url });
+    }
+
+    // remove this entry from storage
+    await chrome.storage.local.remove(snoozeId);
+  } catch (error) {
+    console.error('error handling alarm', snoozeId);
+  }
+});
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -2,7 +2,47 @@ import React from 'react';
 import logo from '../../assets/img/logo.svg';
 import './Popup.css';
 
-const Popup = () => {
+
+function Popup() {
+  function handleSnooze3Min() {
+    // get the active tab
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+      if (!tabs.length) {
+        return;
+      }
+      const activeTab = tabs[0];
+      if (!activeTab.id || !activeTab.url) {
+        return;
+      }
+
+      try {
+        // In snoozeId I also specify "snoozedTab_" because there may be other kinds of settings to save in 
+        // local storage (user preferences), and other kinds of alarms (heartbeat to check no tab re-opens were
+        // missed when browser was closed)
+        const snoozeId =  `snoozedTab_${crypto.randomUUID()}`;
+        const reopenTime =  Date.now() + 3 * 60_000; // 3 minutes from now
+
+        // store the info for this snooze in chrome local storage
+        const snoozeInfo = {
+          url: activeTab.url,
+          reopenAt: reopenTime
+        };
+        await chrome.storage.local.set({ [snoozeId]: snoozeInfo });
+
+        // create alarm
+        chrome.alarms.create(snoozeId, {
+          when: reopenTime
+        });
+
+        // close the tab
+        chrome.tabs.remove(activeTab.id);
+
+      } catch (error) {
+        console.error('errror snoozing tab:', error);
+      }
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +58,13 @@ const Popup = () => {
         >
           Learn React!
         </a>
+
+        <button onClick={handleSnooze3Min}>
+          Snooze for 3 Minutes
+        </button>
       </header>
     </div>
   );
-};
+}
 
 export default Popup;
