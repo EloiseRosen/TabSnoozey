444efc50c1f5b0bce2dd4f5a6a3442e1a02092ba
EloiseRosen <eloise.rosen@gmail.com>
2025-05-13 19:30:02 -0700

    snoozed tabs page functionality and appearance

--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -1,6 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import './Options.css';
+import tabSnoozeyIcon from '../../assets/img/icon-128.png';
 import coffeeMugIcon from '../../assets/img/coffee-mug.svg';
+import deleteIcon from '../../assets/img/delete.svg';
+
+
+/** Format the reopen time nicely.
+ *  If the reopen time is more than one year away show in format  "Fri, May 30, 2026".
+ *  If the reopen time is <= year away show in format like "Fri, May 30, 3:15 PM", using 
+ * user’s 12/24-hour preference.
+ */
+function formatReopenTime(reopenTimeMS: number): string {
+  const reopenDateObj = new Date(reopenTimeMS);
+
+  // determine if tab reopens more than a year from now. if so we will show year instead of time
+  const MS_IN_ONE_YEAR = 365 * 24 * 60 * 60 * 1000;
+  const isMoreThan1YearAway = (reopenTimeMS - Date.now()) > MS_IN_ONE_YEAR;
+
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+  };
+  // if more than a year away, show the year, otherwise show time
+  if (isMoreThan1YearAway) {
+    dateOptions.year = 'numeric';
+  } else {
+    dateOptions.hour = 'numeric';
+    dateOptions.minute = '2-digit';
+
+    // below commented out to use user’s 12/24-hour preference instead
+    // dateOptions.hour12 = true; 
+  }
+
+  // format the date using the specified options
+  return new Intl.DateTimeFormat(undefined, dateOptions).format(reopenDateObj);
+}
 
 
 interface SnoozedTab {
@@ -23,10 +58,12 @@ const Options: React.FC = () => {
 
       for (const [key, val] of Object.entries(allItems)) {
         if (key.startsWith('snoozedTab_')) {
-          const { url, title, reopenAt } = val as { url: string; title: string, reopenAt: number };
+          const { url, title, reopenAt } = val as { url: string; title: string; reopenAt: number };
           tabList.push({ id: key, url: url, title: title, reopenAt: reopenAt });
         }
       }
+
+      tabList.sort((a, b) => a.reopenAt - b.reopenAt);
 
       setSleepingTabsList(tabList);
     } catch (error) {
@@ -72,29 +109,59 @@ const Options: React.FC = () => {
     }
   }
 
+  /**
+   * Delete a snoozed tab.
+   */
+  async function handleDeleteSnooze(id: string) {
+    try {
+      await chrome.storage.local.remove(id);
+      await updateSnoozedTabsList();
+    } catch (error) {
+      console.error('error deleting snooze:', error);
+    }
+  }
+
   return (
     <div className="sleeping-tabs-container">
-      <h1>Snoozed Tabs</h1>
+      <h1 className="page-heading">
+        <img src={tabSnoozeyIcon} alt="Tab Snoozey icon" />
+        Snoozed Tabs
+      </h1>
 
-      {sleepingTabsList.length === 0 ? (
-        <p>No sleeping tabs.</p>
-      ) : (
-        <ul>
-          {sleepingTabsList.map(tab => (
-            <li key={tab.id}>
-              <strong>{tab.title || tab.url}</strong>
-              <br />
-              Wakes at: <em>{new Date(tab.reopenAt).toLocaleString()}</em>
-              <br />
+      <ul>
+        {sleepingTabsList.map(tab => (
+          <li key={tab.id} className="snoozed-item-card">
 
-              <button className="coffee-button" onClick={() => handleWakeNow(tab.id)}>
-                <img src={coffeeMugIcon} alt="coffee mug" />
-              </button>
+            <div className="left-side-of-card">
+              {tab.title || tab.url}
+            </div>
 
-            </li>
-          ))}
-        </ul>
-      )}
+            <div className="right-side-of-card">
+              <div className="wake-time-text">
+                {formatReopenTime(tab.reopenAt)}
+              </div>
+
+              <div>
+                <button
+                  className="coffee-button"
+                  onClick={() => handleWakeNow(tab.id)}
+                  title="Wake now"
+                >
+                  <img src={coffeeMugIcon} alt="coffee mug" />
+                </button>
+                <button
+                  className="delete-button"
+                  onClick={() => handleDeleteSnooze(tab.id)}
+                  title="Delete snooze"
+                >
+                  <img src={deleteIcon} alt="delete X" />
+                </button>
+              </div>
+
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
